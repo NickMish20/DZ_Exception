0c98ab4e3a9bd086ff5ca981a9ebb54e8c2265fe
NickMish <nickmish@mail.ru>
2023-09-03 21:10:50 +0300

    1

---
+++ DZ_2_1/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число больше ноля: ");
+        try {
+            System.out.println(validNumber(scanner.nextInt()));
+            scanner.close();
+        } catch (InvalidNumberException e) {
+            System.out.println(e.getMessage());
+        }
+        scanner.close();
+    }
+
+    private static String validNumber(int number) throws InvalidNumberException {
+        if (number <= 0) throw new InvalidNumberException("Некорректное число");
+        return "Число корректно";
+    }
+}
